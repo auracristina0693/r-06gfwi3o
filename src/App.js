3735ff1e3bf506008d5af2094edfe966bb21245e
Aura Cristina <auracristina0693@gmail.com>
2021-05-18 18:47:15 -0500

    h1-name

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import Welcome from "./Welcome";
+
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      names: ["Juan", "Pedro", "Germán"]
-    }
+      names: ["Juan", "Pedro", "Germán"],
+    };
   }
 
   render() {
     return (
       <div>
-        {this.state.names.map((name) =>
+        {this.state.names.map((name) => (
           <Welcome name={name} />
-        )}
+        ))}
       </div>
     );
   }
